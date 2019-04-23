21ba559c2ab05148f815e580f2002cfe35f1dc19
jrojer <jrojer@example.com>
2019-04-23 22:18:20 +0300

    accepted

--- trees/heap/siftup.cpp
+++ trees/heap/siftup.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
+//Задача №1164. Увеличение приоритета. PDF стр. 9, задача 1.
+//https://informatics.msk.ru/mod/statements/view.php?id=1234#1
+
+
+template<typename T>
+void swap(T& a,T& b)
+{
+    T t  = a;
+    a = b;
+    b = t;
+}
 
 class Heap
 {
     public:
-    Heap(size_t size): size_(size), array_(new int[size]())
+    Heap(size_t size): size_(0), array_(new int[size+1]())
     {
     }
     ~Heap()
@@ -13,22 +24,34 @@ class Heap
     }
     void Insert(int v)
     {
-
+        array_[++size_] = v;
+        SiftUp(size_);
     }
     void Print()
     {
-
+        size_t i = 1;
+        for(; i < size_; ++i)
+        {
+            printf("%d ",array_[i]);
+        }
+        printf("%d\n",array_[i]);
     }
     int Change(int i, int x)
     {
-
+        array_[i] += x;
+        return SiftUp(i);
     }
 
 private:
 
-    int SiftUp()
+    int SiftUp(int i)
     {
-
+        while(i/2 > 0 && array_[i/2] < array_[i])
+        {
+            swap(array_[i/2], array_[i]);
+            i/=2;
+        }
+        return i;
     }
 
     size_t size_;
