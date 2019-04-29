f5e76321a56435eea5ee1c2a46784cbbe28613d3
Rusanov Aleksei <aleksei.s.rusanov@yandex.ru>
2019-04-29 17:53:47 +0300

    added prime factorization

---
+++ computation/factorization/factorize.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> FastFactorize(int n)
+{
+    std::vector<int> res;
+    int k = n;
+    int l = 2;
+    while(k > 1)
+    {
+        while(k%l > 0)
+        {
+            ++l;
+        }
+        res.push_back(l);
+        k /= l;
+    }
+    return res;
+}
+
+
+void PrintArray(const std::vector<int>& array)
+{
+    size_t i = 0;
+    for(; i+1 < array.size(); ++i)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("%d\n", array[i]);
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    PrintArray(FastFactorize(n));
+    return 0;
+}
